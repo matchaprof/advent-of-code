0b942dc2fd09d9e3dc000df4d1bd352492052510
Jonathan <6054441+matchaprof@users.noreply.github.com>
2024-12-04 19:13:45 -0500

    initial day4 commit

--- go/04-ceres-search/main.go
+++ go/04-ceres-search/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func openFileScanner(filename string) (*os.File, *bufio.Scanner, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to open file '%s': %v", filename, err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	return file, scanner, nil
+}
+
 func main() {
-	
+	inputFilename := "input.txt"
+
+	file, scanner, err := openFileScanner(inputFilename)
+	if err != nil {
+		fmt.Errorf("failed to open input file: %v", err)
+	}
+	defer file.Close()
+
+	lineNumber := 0
+	for scanner.Scan() {
+		lineNumber++
+		line := scanner.Text()
+		fmt.Println(line)
+	}
 }
